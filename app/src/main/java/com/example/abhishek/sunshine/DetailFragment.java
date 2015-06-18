d88f121188d73170ad2beb425a81abdc10bf887f
Abhishek Tyagi <Abhishek.tyagi.16@gmail.com>
2015-06-18 15:13:37 +0530

    Files updated

---
+++ app/src/main/java/com/example/abhishek/sunshine/DetailFragment.java
@@ -0,0 +1,7 @@
+package com.example.abhishek.sunshine;
+
+/**
+ * Created by abhishek on 2/6/15.
+ */
+public class DetailFragment {
+}
---
+++ app/src/main/java/com/example/abhishek/sunshine/service/SunshineService.java
@@ -0,0 +1,7 @@
+package com.example.abhishek.sunshine.service;
+
+/**
+ * Created by Abhishek on 26-05-2015.
+ */
+public class SunshineService {
+}
